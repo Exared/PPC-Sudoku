754cfbac2d14a93301ddc48e562bea794d3d3bd5
antho <antho@DESKTOP-6RQBOFB>
2023-04-05 17:22:09 +0200

    First implementation of solver

--- Sudoku.SimulatedAnnealing/Resources/SAPythonSolver.py
+++ Sudoku.SimulatedAnnealing/Resources/SAPythonSolver.py
@@ -7,7 +7,67 @@
 #           (0,7,0,0,0,0,5,2,0),
 #           (9,0,0,0,6,5,0,0,0),
 #           (0,4,0,9,7,0,0,0,0))
-r=instance
+def find_empty_location(puzzle):
+        for row in range(9):
+            for col in range(9):
+                if puzzle[row][col] == 0:
+                    return row, col
+        return None
+
+
+def is_valid(puzzle, row, col, num):
+        # check row
+        for i in range(9):
+            if puzzle[row][i] == num:
+                return False
+        # check column
+        for i in range(9):
+            if puzzle[i][col] == num:
+                return False
+        # check square
+        square_row = (row // 3) * 3
+        square_col = (col // 3) * 3
+        for i in range(3):
+            for j in range(3):
+                if puzzle[square_row + i][square_col + j] == num:
+                    return False
+        return True
+
+
+def solveSudoku(board):
+    """
+    Solves a Sudoku puzzle using a recursive backtracking algorithm.
+
+    Args:
+    - board: 2D list representing the Sudoku board.
+
+    Returns:
+    - True if a solution is found, False otherwise.
+    """
+    # Find the next empty cell
+    row, col = findEmptyCell(board)
+    
+    # If there are no empty cells, the board is solved
+    if row == -1 and col == -1:
+        return True
+    
+    # Try all numbers from 1 to 9 in the empty cell
+    for num in range(1, 10):
+        # Check if the number is valid in the current cell
+        if isValid(board, row, col, num):
+            # If the number is valid, set it in the current cell
+            board[row][col] = num
+            
+            # Recursively solve the updated board
+            if solveSudoku(board):
+                return True
+            
+            # If a solution is not found, backtrack by resetting the current cell
+            board[row][col] = 0
+            
+    # If no valid number is found, backtrack
+    return False
+
 
 #start = default_timer()
 if(solveSudoku(instance)):
